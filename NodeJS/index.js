b9400fd9f55feb67dd1b05af73dc68c25b3b9753
andoreyrich <andoreyrich@gmail.com>
2020-08-16 00:36:06 +0200

    learning Node JS

---
+++ NodeJS/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let i = 1000;
+
+const func = setInterval(function () {
+    i++
+    i < 1015 ? console.log(i) : clearInterval(func)
+}, 1000)
+
+console.log(`%cHi my master!`, 'color: cyan; font-size: 30px')
+
+for (let i = 1, j = 1; j < 8; i < 5 ? ++i : 5, j++){
+    setTimeout(function () {
+        console.log('number ' + i)
+    }, 2000 * j)
+}
+
+
